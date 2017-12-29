867a5063716b7c49d58e008e229a4417abfbef9d
Anthony PIRON <anthony.piron@skynet.be>
2017-12-29 21:44:04 +0100

    part1

--- src/part1.py
+++ src/part1.py
@@ -26,10 +26,20 @@ class LJALNPart1(LJALPart1):
 
     def __init__(self, n_out = 0):
         """
-        n_out: number of out edges. n_out = 0 is IL. n_out = 2 is LJAL-2
+        n_out: number of out edges. n_out = 0 is IL. n_out = 3 is LJAL-3
         """
         graph = Graph(5)
         for n in range(0,5):
             for i in range(0, n_out):
                 graph.add_arc(n, numpy.random.randint(0, 5))
         super(LJAL2Part1, self).__init__(graph=graph)
+
+class JALPart1(LJALPart1):
+
+    def __init__(self):
+        graph = Graph(5)
+        for n1 in range(0,5):
+            for n2 in range(0,5):
+                if n1 != n2:
+                    graph.add_arc(n1, n2)
+        super(JAL2Part1, self).__init__(graph=graph)
