971d32a3921161b1c35536dd454635fff80453b7
Anthony PIRON <anthony.piron@skynet.be>
2017-12-29 21:31:43 +0100

    more testing

---
+++ src/part1.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+import unittest
+import numpy as np
+
+from graph import Graph
+from ljal import LJAL
+
+
+class LJALPart1(LJAL):
+
+    def __init__(self, graph):
+        super(LJALPart1, self).__init__(graph=graph, n_actions = 4)
+        # self.rewards = np.
+
+    def temperature(self):
+        return 1000 * 0.94**self.step
+
+    def reward(self, actions):
+        # use self.reward[actions]
+        return 1
+
+    
+
+class ILPart1(LJALPart1):
+
+    def __init__(self):
+        graph = Graph(5)
+        super(ILPart1, self).__init__(graph=graph)
+
+        
+class LJAL2Part1(LJALPart1):
+
+    def __init__(self):
+        graph = Graph(5)
+        for n in range(0,5):
+            graph.add(n, numpy.random.randint(0, 5))
+        super(LJAL2Part1, self).__init__(graph=graph)
