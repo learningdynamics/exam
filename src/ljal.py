Fix BoltzmannAction overflow + save graph to file

src/ljal.py
@@ -2,15 +2,17 @@
 
 import unittest
 import numpy as np
-from joblib import Parallel, delayed
-from multiprocessing import Pool
 
 from graph import *
 
 def BoltzmannAction(evs, temp=1):
+    # Prevent overflow
+    temp = max(temp, 0.01)
     cs = np.cumsum(np.exp(np.array(evs)/temp))
-    rd = np.random.uniform(0,cs[-1])
-    return len(cs) - np.sum(rd <= cs)
+    cs = cs / cs[-1]
+    rd = np.random.uniform()
+    l = len(cs)
+    return min(l - np.sum(rd <= cs), l-1)
 
 def EVs(Q, N):
     ## np.average(Q, axis=1, weigths=N) but handling the case where

src/part1.py
@@ -4,6 +4,10 @@ import unittest
 import numpy as np
 import matplotlib.pyplot as plt
 
+from timeit import default_timer as timer
+from multiprocessing import Pool
+import sys
+
 from graph import (Graph, RandomGraph, FullGraph)
 from ljal import LJAL, AverageR
 
@@ -16,8 +20,8 @@ from ljal import LJAL, AverageR
 
 class LJALPart1(LJAL):
 
-    def __init__(self, graph):
-        super(LJALPart1, self).__init__(graph=graph, n_actions = 4)
+    def __init__(self, graph, alpha=0.1):
+        super(LJALPart1, self).__init__(graph=graph, n_actions = 4, alpha = alpha)
         self.rewards = np.reshape(np.random.normal(0,50,self.n_actions**self.n_agents),
                                   [self.n_actions for i in range(0, self.n_agents)])
 
@@ -28,41 +32,42 @@ class LJALPart1(LJAL):
     def reward(self, actions):
         return self.rewards[tuple(actions)]
 
-        
-class LJALNPart1(LJALPart1):
 
-    def __init__(self, n_out = 0):
-        """
-        n_out: number of out edges. 
-               n_out: 0 is IL 
-                    : 2 is LJAL-2
-                    : 3 is LJAL-3
-
-        """
-        graph = RandomGraph(5, n_out)
-        super(LJALNPart1, self).__init__(graph=graph)
-
-        
-class JALPart1(LJALPart1):
-
-    def __init__(self):
-        graph = FullGraph(5)
-        super(JALPart1, self).__init__(graph=graph)
-
+n_samples = 10
+if len(sys.argv) > 1:
+    n_samples = int(sys.argv[1])
 
 steps = 200
 index = [i for i in range(1, steps+1)]
-IL = AverageR(20, lambda:LJALNPart1().n_steps(steps))
-LJAL_2 = AverageR(20, lambda:LJALNPart1(n_out = 2).n_steps(steps))
-LJAL_3 = AverageR(20, lambda:LJALNPart1(n_out = 3).n_steps(steps))
-JAL = AverageR(20, lambda:JALPart1().n_steps(steps))
 
-print(len(index))
-print(len(IL))
-print(len(LJAL_2))
-print(len(LJAL_3))
-print(len(JAL))
+start = timer()
+IL = AverageR(n_samples, lambda:LJALPart1(graph=RandomGraph(5, 0)).n_steps(steps))
+end = timer()
+IL_delta = end - start
 
+start = timer()
+LJAL_2 = AverageR(n_samples, lambda:LJALPart1(graph=RandomGraph(5, 2)).n_steps(steps))
+end = timer()
+LJAL_2_delta = end - start
+
+
+start = timer()
+LJAL_3 = AverageR(n_samples, lambda:LJALPart1(graph=RandomGraph(5, 3)).n_steps(steps))
+end = timer()
+LJAL_3_delta = end - start
+
+start = timer()
+JAL = AverageR(n_samples, lambda:LJALPart1(graph=FullGraph(5)).n_steps(steps))
+end = timer()
+JAL_delta = end - start
+
+
+timing = np.array([IL_delta,  LJAL_2_delta, LJAL_3_delta, JAL_delta]) / JAL_delta
+print( timing )
+
+
+plt.ylim(-10, 120)
 plt.plot(index, IL, 'r', index, LJAL_2, 'b', index, LJAL_3, 'g', index, JAL, 'y')
 plt.ylabel('R')
-plt.show()
+plt.savefig('part1.png')
+#plt.show()