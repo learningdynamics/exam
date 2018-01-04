96e81525a23a924cdd99d12249b3806144b3e85b
Anthony PIRON <anthony.piron@skynet.be>
2018-01-05 00:58:38 +0100

    Add hydra script

---
+++ src/hydra_job_part1.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=00:10:00
+#PBS -o myjob.out
+#PBS -N Part1_Job
+
+echo "Running job on $HOST - " `date`
+
+module load Python matplotlib
+
+python3 $HOME/git/exam/src/ -n 10 --plot $WORKDIR/part1_plot.png --latex $WORKDIR/part1_latex.tex
+
+echo "Done"
